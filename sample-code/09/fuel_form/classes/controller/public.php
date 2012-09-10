39ff20f06001c33834f3d297fb34aa9f35b79e71
kenjis <kenji.uui@gmail.com>
2012-09-10 09:34:57 +0900

    FuelPHP 1.3 対応のための変更

--- sample-code/09/fuel_form/classes/controller/public.php
+++ sample-code/09/fuel_form/classes/controller/public.php
@@ -15,6 +15,14 @@ class Controller_Public extends Controller_Template
 	public function before()
 	{
 		parent::before();
+		$this->response = new Response();
 		$this->response->set_header('X-FRAME-OPTIONS', 'SAMEORIGIN');
+	}
+	
+	public function after($response)
+	{
+		$response = $this->response;
+		$response->body = $this->template;
+		return parent::after($response);
 	}
 }
