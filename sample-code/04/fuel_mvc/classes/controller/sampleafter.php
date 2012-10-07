264071ef0234f05ff4cf869ce5d316376c3b8103
kenjis <kenji.uui@gmail.com>
2012-10-07 18:13:39 +0900

    FuelPHP 1.3 対応のための変更

--- sample-code/04/fuel_mvc/classes/controller/sampleafter.php
+++ sample-code/04/fuel_mvc/classes/controller/sampleafter.php
@@ -15,7 +15,7 @@ class Controller_SampleAfter extends Controller
 	public function after($response)
 	{
 		$response .= __METHOD__ . 'が実行されました。<br />';
-		return $response;
+		return parent::after($response);
 	}
 	
 	public function action_index()
